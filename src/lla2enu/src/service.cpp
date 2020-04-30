1ea10049df818dcc78213c468619a4791143f0a7
Mattereteo <matteo3.montanari@mail.polimi.it>
2020-04-30 09:01:47 -0400

    made service into a class

--- src/lla2enu/src/service.cpp
+++ src/lla2enu/src/service.cpp
@@ -2,60 +2,83 @@
 #include "geometry_msgs/Vector3Stamped.h"
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
-
-
-void callback(const geometry_msgs::Vector3StampedConstPtr& msg_front, const geometry_msgs::Vector3StampedConstPtr& msg_obs)
+class Service
 {
-
-	float xEastFront_ = msg_front -> vector.x;
-	float yNorthFront_ = msg_front -> vector.y;
-	float zUpFront_ = msg_front -> vector.z;
-
- 	float xEastObs_ = msg_obs -> vector.x;
-	float yNorthObs_ = msg_obs -> vector.y;
-	float zUpObs_ = msg_obs -> vector.z;
-
-	float unsafe = 2;	
-	float crash = 3;
-
-	// n_.getParam("/unsafe", unsafe);
-	// n_.getParam("/crash", crash);
-	
-	if(!isnan(xEastObs_) && !isnan(xEastFront_)){
+public:
+	Service()
+	{	
 		
-		float result = sqrt(pow((xEastFront_ - xEastObs_), 2) + pow((yNorthFront_ - yNorthObs_), 2) + pow((zUpFront_ - zUpObs_), 2)); 
+
+		sub_front_.subscribe(n_, "/front_enu", 10000);
+		sub_obs_.subscribe(n_, "/obs_enu", 10000);
 		
-		if(result > unsafe)
-			ROS_INFO("SAFE %f", result);
-		else if (result > crash)
-			ROS_INFO("UNSAFE %f", result);
-		else
-			ROS_INFO("CRASH %f", result);
-	}
-	else {
-		ROS_INFO("obs value still missing");
-	}
-	
-	
+		sync.reset(new Sync(MySyncPolicy(10), sub_front_, sub_obs_));  
 
-}
 
+		sync->registerCallback(boost::bind(&Service::callback, this, _1, _2));
+
+		ROS_INFO("buildato");
+	}
+
+
+	void callback(const geometry_msgs::Vector3StampedConstPtr& msg_front, const geometry_msgs::Vector3StampedConstPtr& msg_obs)
+	{
+
+		float xEastFront_ = msg_front -> vector.x;
+		float yNorthFront_ = msg_front -> vector.y;
+		float zUpFront_ = msg_front -> vector.z;
+
+	 	float xEastObs_ = msg_obs -> vector.x;
+		float yNorthObs_ = msg_obs -> vector.y;
+		float zUpObs_ = msg_obs -> vector.z;
+
+		float unsafe = 2;	
+		float crash = 3;
+
+		n_.getParam("/unsafe", unsafe);
+		n_.getParam("/crash", crash);
+		
+		if(!isnan(xEastObs_) && !isnan(xEastFront_)){
+			
+			float result = sqrt(pow((xEastFront_ - xEastObs_), 2) + pow((yNorthFront_ - yNorthObs_), 2) + pow((zUpFront_ - zUpObs_), 2)); 
+			
+			if(result > unsafe)
+				ROS_INFO("SAFE %f", result);
+			else if (result > crash)
+				ROS_INFO("UNSAFE %f", result);
+			else
+				ROS_INFO("CRASH %f", result);
+		}
+		else {
+			ROS_INFO("NaN");
+		}
+		
+		
+
+	}
+
+private:
+
+	ros::NodeHandle n_; 
+	message_filters::Subscriber<geometry_msgs::Vector3Stamped> sub_front_;
+	message_filters::Subscriber<geometry_msgs::Vector3Stamped> sub_obs_;
+	typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::Vector3Stamped, geometry_msgs::Vector3Stamped> MySyncPolicy;
+
+	typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+
+	boost::shared_ptr<Sync> sync;
+
+};//End
 
 int main(int argc, char** argv)
 {
 
 	ros::init(argc, argv, "subscriber");
-	ros::NodeHandle n_; 
-	message_filters::Subscriber<geometry_msgs::Vector3Stamped> sub_front(n_, "/front_enu", 10000);
-	message_filters::Subscriber<geometry_msgs::Vector3Stamped> sub_obs(n_, "/obs_enu", 10000);
-
-	typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::Vector3Stamped, geometry_msgs::Vector3Stamped> MySyncPolicy;
-	message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(100000), sub_front, sub_obs);
-	sync.registerCallback(boost::bind(&callback, _1, _2));
-
+	
+	Service service;
 
 	ros::spin();
 
--- src/lla2enu/src/subscriber_front.cpp
+++ src/lla2enu/src/subscriber_front.cpp
@@ -9,131 +9,131 @@
 class SubscribeAndPublish
 {
 public:
-  SubscribeAndPublish(std::string arg)
-  {
-    //n_.getParam("/role", role);
-    role_ = arg.c_str();
+	SubscribeAndPublish(std::string arg)
+	{
+		//n_.getParam("/role", role);
+		role_ = arg.c_str();
 
-    //ROS_INFO("role: [%s]", role_.c_str());
-    //Topic you want to publish
-    pub_ = n_.advertise<geometry_msgs::Vector3Stamped>("/" + role_ + "_enu", 1000);
+		//Topic you want to publish
+		pub_ = n_.advertise<geometry_msgs::Vector3Stamped>("/" + role_ + "_enu", 1000);
 
-    //Topic you want to subscribe
-    sub_ = n_.subscribe("/swiftnav/"+ role_ + "/gps_pose", 1000, &SubscribeAndPublish::callback, this);
-  }
-
-
-  void callback(const sensor_msgs::NavSatFix::ConstPtr& msg){
-    //ROS_INFO("Input position: [%f,%f, %f]", msg->latitude, msg->longitude,msg->altitude);
-
-    // fixed values
-
-    double a = 6378137;
-    double b = 6356752.3142;
-    double f = (a - b) / a;
-    double e_sq = f * (2-f);
-    float deg_to_rad = 0.0174533;
-
-    // input data from msg
-    float latitude = msg->latitude;
-    float longitude = msg->longitude;
-    float h = msg->altitude;
-
-    ros::Time current_time = msg -> header.stamp;
-
-    geometry_msgs::Vector3Stamped output;
- 
-    // if the message is formed of only 0, we publish an empty message since it means the gps has dropped
-    if (latitude == 0 && longitude == 0 && h == 0){
-	output.header.stamp = current_time;
-	output.header.frame_id = "odom";
-    	output.vector.x = NAN;
-    	output.vector.y = NAN;
-    	output.vector.z = NAN;
-    	pub_.publish(output);
-	return;
-    }
-
-    // fixed position
-    float latitude_init = 0.0;
-    float longitude_init = 0.0;
-    float h0 = 0.0;
-    n_.getParam("/starting_latitude_init", latitude_init);
-    n_.getParam("/starting_longitude_init", longitude_init);
-    n_.getParam("/starting_h0_init", h0);
+		//Topic you want to subscribe
+		sub_ = n_.subscribe("/swiftnav/"+ role_ + "/gps_pose", 1000, &SubscribeAndPublish::callback, this);
+	}
 
 
-    //lla to ecef
-    float lamb = deg_to_rad*(latitude);
-    float phi = deg_to_rad*(longitude);
-    float s = sin(lamb);
-    float N = a / sqrt(1 - e_sq * s * s);
+	void callback(const sensor_msgs::NavSatFix::ConstPtr& msg)
+	{
+		//ROS_INFO("Input position: [%f,%f, %f]", msg->latitude, msg->longitude,msg->altitude);
 
-    float sin_lambda = sin(lamb);
-    float  cos_lambda = cos(lamb);
-    float  sin_phi = sin(phi);
-    float  cos_phi = cos(phi);
+		// fixed values
 
-    float  x = (h + N) * cos_lambda * cos_phi;
-    float  y = (h + N) * cos_lambda * sin_phi;
-    float  z = (h + (1 - e_sq) * N) * sin_lambda;
+		double a = 6378137;
+		double b = 6356752.3142;
+		double f = (a - b) / a;
+		double e_sq = f * (2-f);
+		float deg_to_rad = 0.0174533;
 
-    //ROS_INFO("ECEF position: [%f,%f, %f]", x, y,z);
+		// input data from msg
+		float latitude = msg->latitude;
+		float longitude = msg->longitude;
+		float h = msg->altitude;
+
+		ros::Time current_time = msg -> header.stamp;
+
+		geometry_msgs::Vector3Stamped output;
+
+		// if the message is formed of only 0, we publish an empty message since it means the gps has dropped
+		if (latitude == 0 && longitude == 0 && h == 0){
+		output.header.stamp = current_time;
+		output.header.frame_id = "odom";
+		output.vector.x = NAN;
+		output.vector.y = NAN;
+		output.vector.z = NAN;
+		pub_.publish(output);
+		return;
+		}
+
+		// fixed position
+		float latitude_init = 0.0;
+		float longitude_init = 0.0;
+		float h0 = 0.0;
+		n_.getParam("/starting_latitude_init", latitude_init);
+		n_.getParam("/starting_longitude_init", longitude_init);
+		n_.getParam("/starting_h0_init", h0);
 
 
-    // ecef to enu
+		//lla to ecef
+		float lamb = deg_to_rad*(latitude);
+		float phi = deg_to_rad*(longitude);
+		float s = sin(lamb);
+		float N = a / sqrt(1 - e_sq * s * s);
 
-    lamb = deg_to_rad*(latitude_init);
-    phi = deg_to_rad*(longitude_init);
-    s = sin(lamb);
-    N = a / sqrt(1 - e_sq * s * s);
+		float sin_lambda = sin(lamb);
+		float  cos_lambda = cos(lamb);
+		float  sin_phi = sin(phi);
+		float  cos_phi = cos(phi);
 
-    sin_lambda = sin(lamb);
-    cos_lambda = cos(lamb);
-    sin_phi = sin(phi);
-    cos_phi = cos(phi);
+		float  x = (h + N) * cos_lambda * cos_phi;
+		float  y = (h + N) * cos_lambda * sin_phi;
+		float  z = (h + (1 - e_sq) * N) * sin_lambda;
 
-    float  x0 = (h0 + N) * cos_lambda * cos_phi;
-    float  y0 = (h0 + N) * cos_lambda * sin_phi;
-    float  z0 = (h0 + (1 - e_sq) * N) * sin_lambda;
+		//ROS_INFO("ECEF position: [%f,%f, %f]", x, y,z);
 
-    float xd = x - x0;
-    float  yd = y - y0;
-    float  zd = z - z0;
 
-    float  xEast = -sin_phi * xd + cos_phi * yd;
-    float  yNorth = -cos_phi * sin_lambda * xd - sin_lambda * sin_phi * yd + cos_lambda * zd;
-    float  zUp = cos_lambda * cos_phi * xd + cos_lambda * sin_phi * yd + sin_lambda * zd;
+		// ecef to enu
 
-    //ROS_INFO("ENU position: [%f,%f, %f]", xEast, yNorth,zUp);
+		lamb = deg_to_rad*(latitude_init);
+		phi = deg_to_rad*(longitude_init);
+		s = sin(lamb);
+		N = a / sqrt(1 - e_sq * s * s);
 
-    // preparing the message to publish
-    output.header.stamp = current_time;
-    output.header.frame_id = role_ + "_enu";
-    output.vector.x = xEast;
-    output.vector.y = yNorth;
-    output.vector.z = zUp;
-    pub_.publish(output);
-  }
+		sin_lambda = sin(lamb);
+		cos_lambda = cos(lamb);
+		sin_phi = sin(phi);
+		cos_phi = cos(phi);
+
+		float  x0 = (h0 + N) * cos_lambda * cos_phi;
+		float  y0 = (h0 + N) * cos_lambda * sin_phi;
+		float  z0 = (h0 + (1 - e_sq) * N) * sin_lambda;
+
+		float xd = x - x0;
+		float  yd = y - y0;
+		float  zd = z - z0;
+
+		float  xEast = -sin_phi * xd + cos_phi * yd;
+		float  yNorth = -cos_phi * sin_lambda * xd - sin_lambda * sin_phi * yd + cos_lambda * zd;
+		float  zUp = cos_lambda * cos_phi * xd + cos_lambda * sin_phi * yd + sin_lambda * zd;
+
+		//ROS_INFO("ENU position: [%f,%f, %f]", xEast, yNorth,zUp);
+
+		// preparing the message to publish
+		output.header.stamp = current_time;
+		output.header.frame_id = role_ + "_enu";
+		output.vector.x = xEast;
+		output.vector.y = yNorth;
+		output.vector.z = zUp;
+		pub_.publish(output);
+	}
 
 private:
-  ros::NodeHandle n_; 
-  ros::Publisher pub_;
-  ros::Subscriber sub_;
-  
-  std::string role_;
+	ros::NodeHandle n_; 
+	ros::Publisher pub_;
+	ros::Subscriber sub_;
+
+	std::string role_;
 
 };//End of class
 
 int main(int argc, char **argv)
 {
-  //Initiate ROS
-  ros::init(argc, argv, "listener");
+	//Initiate ROS
+	ros::init(argc, argv, "listener");
 
-  //Create an object of class 
-  SubscribeAndPublish SAPObject(argv[1]);
+	//Create an object of class 
+	SubscribeAndPublish SAPObject(argv[1]);
 
-  ros::spin();
+	ros::spin();
 
-  return 0;
+	return 0;
 }
